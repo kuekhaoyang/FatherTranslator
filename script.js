ecd0aa72d557a65b9f79545dc12b89c0237963ed
fatherxtreme123 <thunderstrikelord@gmail.com>
2024-07-24 08:19:39 +0800

    More Model support

--- script.js
+++ script.js
@@ -35,8 +35,9 @@ document.addEventListener('DOMContentLoaded', () => {
             'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-16k-0613'
         ],
         groq: [
-            'llama3-8b-8192', 'llama3-70b-8192', 'llama3-groq-8b-8192-tool-use-preview',
-            'llama3-groq-70b-8192-tool-use-preview', 'mixtral-8x7b-32768', 'gemma-7b-it', 'gemma2-9b-it'
+            'llama-3.1-405b-reasoning', 'llama-3.1-70b-versatile', 'llama-3.1-8b-instant',
+            'llama3-groq-70b-8192-tool-use-preview', 'llama3-groq-8b-8192-tool-use-preview',
+            'llama3-70b-8192', 'llama3-8b-8192', 'mixtral-8x7b-32768', 'gemma-7b-it', 'gemma2-9b-it'
         ]
     };
 
